4a78a6ff34b25369537276ef2d286d9379dafce5
Raphael Correia Dornelas <dornelasrapha@gmail.com>
2024-06-27 22:21:30 -0300

    atl

---
+++ aula12/arv.c
@@ -0,0 +1,4 @@
+/*mplementar uma árvore com número variável de filhos, como nos slides.
+Além das funcionalidades básicas (criação, impressão, busca, liberação), implementar as seguintes funcionalidades: 
+calcular a altura da árvore; calcular o número de nós folha de uma árvore; 
+e calcular o número de nós com apenas um filho.*/
